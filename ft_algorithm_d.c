6b034df87ed2cf33d071d4792d55dc5f22733a19
Sandile Mkhize <hellosandile@Sandiles-MacBook-Pro.local>
2020-02-19 16:19:32 +0200

    clean update

--- ft_algorithm_d.c
+++ ft_algorithm_d.c
@@ -1,22 +1,18 @@
 #include "push_swap.h"
 #include <stdio.h>
 
-
 void	sortFive(stack_a **a, stack_b **b)
 {
 	stack_a *list;
 	stack_b *listb;
 	list = *a;
 	listb = *b;
-
+	
 	while (listsize(list) > 3 && stacksorted(list) == 0)
 		{
 			if (last(&list) == min(&list))
 			{
 				ft_reverse_a(&list, listsize(list));
-				//ft_printlist(list);
-				//printf("%s", "-");
-				//sleep(5);
 				ft_putendl("rra");
 			}
 			 if (list->data != min(&list))
@@ -31,12 +27,28 @@ void	sortFive(stack_a **a, stack_b **b)
 			}
 		}
 		while (stacksorted(list) != 1)
+			sortThree(&list);
+		while (listsize(listb) > 0)
+		{
+			ft_push_a(&list, &listb);
+			ft_putendl("pa");
+		}
+		if (listsize(listb) == 0)
+			listb = NULL;
+		*a = list;
+}
+
+void	sortThree(stack_a **a)
+{
+	stack_a *list;
+	list = *a;
+
+	while (stacksorted(list) != 1)
 		{
 			if (last_g_middle(&list) == 0 && head_g_middle(&list) == 0 && head_g_last(&list) == 1)
 			{
 				ft_reverse_a(&list, 3);
 				ft_putendl("rra");
-				
 			}
 			if (last_g_middle(&list) == 0 && head_g_middle(&list) == 1)
 			{
@@ -53,23 +65,9 @@ void	sortFive(stack_a **a, stack_b **b)
 				ft_swap_a(&list, 2);
 				ft_putendl("sa");
 			}
-			if (head_g_middle(&list) == 0 && last_g_middle(&list) == 0)
-			{
-				ft_swap_a(&list, 2);
-				ft_putendl("sa");
-			}
 		}
-		while (listsize(listb) > 0)
-		{
-			ft_push_a(&list, &listb);
-			ft_putendl("pa");
-			//printf("%p\n", (void *) list->next);
-		}
-		if (listsize(listb) == 0)
-			listb = NULL;
 		*a = list;
 }
-
 
 
 int	main(int ac , char **av)
@@ -77,7 +75,6 @@ int	main(int ac , char **av)
 	char **data;
 	stack_a *list;
 	stack_b *listb;
-
 	list = (stack_a *)malloc(sizeof(stack_a));
 	listb = (stack_b *)malloc(sizeof(stack_b));
 	listb = NULL;
@@ -86,7 +83,9 @@ int	main(int ac , char **av)
     {
 		data = ft_strsplit(av[1], ' ');
 		initialize(&list, data);
-        if (listsize(list) == 5  && stacksorted(list) == 0)
+		if (listsize(list) < 4 && stacksorted(list) == 0)
+			sortThree(&list);
+        if (listsize(list) < 6  && listsize(list) > 3 && stacksorted(list) == 0)
 			sortFive(&list, &listb);	
     }
 printf("Final list contents of stack_a: ");
